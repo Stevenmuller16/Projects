1b67c4e2e10e727ad283c17e9814101e7417f278
Steven Muller <stvnmuller@gmail.com>
2018-12-23 13:51:40 +0200

    adding functionality

--- JS/Intermediate/TODO_List/assets/js/todos.js
+++ JS/Intermediate/TODO_List/assets/js/todos.js
@@ -1,4 +1,18 @@
 // check out items with click event
 $("li").click(function(){
+    
     $(this).toggleClass("completed");
+});
+
+$("span").click(function(e){
+    e.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    
+});
+
+$("input[type='text']").keypress(function(){
+
+
 });
